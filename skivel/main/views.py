00da2fd8a8720c3bf52bbe743b378c0daf1778f4
kerish <denisbuhrov@gmail.com>
2021-04-06 23:00:13 +0300

    update

--- skivel/main/views.py
+++ skivel/main/views.py
@@ -2,9 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def index():
-    pass
-
-
-def about(request):
+def index(request):
     return HttpResponse("<h1>About Page</h1>")
