0197b1b8f90f9f5591856fc1387a5fc506dede2b
skivel <denisbuhrov@gmail.com>
2021-05-08 06:49:48 +0300

    SHOP migrations(add Cart)

--- skivel/shop/models.py
+++ skivel/shop/models.py
@@ -21,4 +21,22 @@ class Product(models.Model):
         return self.title
 
 
+class CartProduct(models.Model):
+    user = models.ForeignKey('Customer', verbose_name='Покупець', on_delete=models.CASCADE)
+    cart = models.ForeignKey('Cart', verbose_name='Корзина', on_delete=models.CASCADE)
+    product = models.ForeignKey('Product', verbose_name='Товар', on_delete=models.CASCADE)
+    unmount = models.PositiveIntegerField(default=1)
+    total_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Загальна Ціна')
 
+    def __str__(self):
+        return "Продукт: {}".format(self.product.title)
+
+
+class Cart(models.Model):
+    owner = models.ForeignKey('Customer', verbose_name='Власник', on_delete=True)
+    product = models.ManyToManyField(CartProduct, blank=True)
+    total_products = models.PositiveIntegerField(default=0)
+    total_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Загальна Ціна')
+
+    def __str__(self):
+        return str(self.id)
