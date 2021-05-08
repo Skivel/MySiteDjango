0c0fcd6579178204abb616ce951f856751f4d503
skivel <denisbuhrov@gmail.com>
2021-05-08 06:49:03 +0300

    SHOP migrations(add Category && Product)

--- skivel/shop/models.py
+++ skivel/shop/models.py
@@ -1,15 +1,24 @@
 from django.db import models
 
 
-class Сargo(models.Model):
-    title = models.CharField('Заголовок', max_length=50)
-    characteristics = models.TextField('Характеристики')
-    price = models.CharField('Ціна', max_length=10)
-    date = models.DateField('Дата')
+class Category(models.Model):
+    name = models.CharField(max_length=255, verbose_name='Назва категорії')
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, verbose_name='Test', on_delete=models.CASCADE)
+    title = models.CharField(max_length=255, verbose_name='Назва товару')
+    slug = models.SlugField(unique=True)
+    image = models.ImageField(verbose_name='Фото')
+    description = models.TextField(verbose_name='Опис', null=True)
+    price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Ціна')
 
     def __str__(self):
         return self.title
 
-    class Meta:
-        verbose_name = 'Товари',
-        verbose_name_plural = 'Товари'
+
+
