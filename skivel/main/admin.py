4954596dbd95526e127d8a1ef578c3fb0b4ed2d8
skivel <denisbuhrov@gmail.com>
2022-07-16 02:33:17 +0200

    create models for Site Management

--- skivel/main/admin.py
+++ skivel/main/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+@admin.register(Me)
+class MeAdmin(admin.ModelAdmin):
+    list_display = ['title', 'status']
+    list_editable = ['status']
+
+
+@admin.register(Skills)
+class SkillsAdmin(admin.ModelAdmin):
+    list_display = ['language', 'skills']
+    list_editable = ['skills']
+
+
+@admin.register(Portfolio)
+class PortfolioAdmin(admin.ModelAdmin):
+    list_display = ['name', 'git_url']
+
+
+@admin.register(ContactMe)
+class ContactMeAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email']
+
--- skivel/main/models.py
+++ skivel/main/models.py
@@ -2,26 +2,51 @@ from django.db import models
 
 
 class Me(models.Model):
+    id = models.AutoField(primary_key=True)
     title = models.CharField(verbose_name='Заговолок', max_length=255)
     description = models.TextField(verbose_name='Анотація')
-    img = models.ImageField()
+    img = models.ImageField(verbose_name='Зображення', upload_to="static/me/img")
+    age = models.IntegerField(verbose_name='Вік')
+    email = models.EmailField(verbose_name='Email')
+    phone = models.CharField(verbose_name='Номер телефону', max_length=255)
+    address = models.CharField(verbose_name='Адрес', max_length=255)
+    status = models.BooleanField(verbose_name='Статус зайнятості')
 
     class Meta:
         verbose_name = 'Me'
         verbose_name_plural = 'Me'
 
 
-class Services(models.Model):
-    pass
-
-
 class Skills(models.Model):
-    pass
+    id = models.AutoField(primary_key=True)
+    language = models.CharField(verbose_name='Назва технології', max_length=144)
+    skills = models.IntegerField(verbose_name='Розуміння %')
+
+    class Meta:
+        verbose_name = 'Skills'
+        verbose_name_plural = 'Skills'
 
 
 class Portfolio(models.Model):
-    pass
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(verbose_name='Назва проекту', max_length=255)
+    description = models.TextField(verbose_name='Опис проекту')
+    img = models.ImageField(verbose_name='Зображення', upload_to="static/portfolio/img")
+    git_url = models.URLField(verbose_name='GitHub URL')
 
 
-class Contact(models.Model):
-    pass
+
+    class Meta:
+        verbose_name = 'Portfolio'
+        verbose_name_plural = 'Portfolio'
+
+
+class ContactMe(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(verbose_name="'Ім'я", max_length=255)
+    email = models.EmailField(verbose_name='Email')
+    massage = models.TextField(verbose_name='Повідомлення')
+
+    class Meta:
+        verbose_name = 'Massage'
+        verbose_name_plural = 'Massage'
